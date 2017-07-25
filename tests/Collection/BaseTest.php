51712e49d992268bb439384bc3ab115484a1f2bd
李铭昕 <715557344@qq.com>
2017-07-25 13:11:27 +0800

    * 增加单元测试

--- tests/Collection/BaseTest.php
+++ tests/Collection/BaseTest.php
@@ -19,6 +19,11 @@ class BaseTest extends TestCase
         'tel' => ['mobile' => '18678017520', 'phone' => '8653623']
     ];
 
+    protected $list = [
+        ['id' => 1, 'name' => 'limx'],
+        ['id' => 2, 'name' => 'Agnes'],
+    ];
+
     public function testInit()
     {
         $arr = ['name' => 'limx', 'sex' => 1];
@@ -36,6 +41,75 @@ class BaseTest extends TestCase
 
     public function testAll()
     {
-        
+        $collection = new Collection($this->arr);
+        $this->assertEquals($this->arr, $collection->all());
+    }
+
+    public function testOnly()
+    {
+        $collection = new Collection($this->arr);
+        $this->assertArrayNotHasKey('tel', $collection->only(['name', 'sex']));
+    }
+
+    public function testExcept()
+    {
+        $collection = new Collection($this->arr);
+        $this->assertArrayNotHasKey('name', $collection->except(['name']));
+    }
+
+    public function testMerge()
+    {
+        $collection = new Collection($this->arr);
+        $new = $collection->merge(['name2' => 'limx2']);
+        $this->assertArrayHasKey('name', $new);
+        $this->assertArrayHasKey('name2', $new);
+    }
+
+    public function testHas()
+    {
+        $collection = new Collection($this->arr);
+        $this->assertTrue($collection->has('name'));
+    }
+
+    public function testFirst()
+    {
+        $collection = new Collection($this->list);
+        $this->assertEquals($this->list[0], $collection->first());
+    }
+
+    public function testLast()
+    {
+        $collection = new Collection($this->list);
+        $this->assertEquals($this->list[1], $collection->last());
+    }
+
+    public function testAdd()
+    {
+        $collection = new Collection($this->arr);
+        $arr = $this->arr;
+
+        $collection->add('name2', 'Agnes');
+        $arr['name2'] = 'Agnes';
+        $this->assertEquals($arr, $collection->all());
+    }
+
+    public function testSet()
+    {
+        $collection = new Collection($this->arr);
+        $arr = $this->arr;
+
+        $collection->set('name', 'Agnes');
+        $arr['name'] = 'Agnes';
+        $this->assertEquals($arr, $collection->all());
+    }
+
+    public function testForget()
+    {
+        $collection = new Collection($this->arr);
+        $arr = $this->arr;
+
+        $collection->forget('name');
+        unset($arr['name']);
+        $this->assertEquals($arr, $collection->all());
     }
 }
